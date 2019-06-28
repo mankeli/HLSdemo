e09212498c28843ed7d61394d0fe659b59452079
mankeli <markus@electrontu.be>
2019-06-28 14:06:35 +0300

    effy improvement and comments

--- hlstest.c
+++ hlstest.c
@@ -52,14 +52,96 @@ void sendframe(SDL_Surface* image)
 
 }
 
+int min(int v1, int v2)
+{
+	return v1 < v2 ? v1 : v2;
+}
 
+int max(int v1, int v2)
+{
+	return v1 > v2 ? v1 : v2;
+}
+
+void gradientrect(SDL_Surface* image, SDL_Rect* rect)
+{
+	int x1 = max(rect->x, 0);
+	int x2 = min(rect->x + rect->w, image->w);
+	int y1 = max(rect->y, 0);
+	int y2 = min(rect->y + rect->h, image->h);
+
+	int rd = 0xc000 / image->h;
+	int gd = 0x6000 / image->h;
+	int bd = 0x5000 / image->h;
+	int ra = 0x4000 + rd * y1;
+	int ga = 0x8000 + gd * y1;
+	int ba = 0x3000 + bd * y1;
+
+
+	for (int y = y1; y < y2; y++)
+	{
+		uint32_t *pixels = (uint32_t *)(image->pixels + y * image->pitch);
+
+		for (int x = x1; x < x2; x++)
+		{
+			int r8 = (ra / 256) & 0xFF;
+			int g8 = (ga / 256) & 0xFF;
+			int b8 = (ba / 256) & 0xFF;
+			pixels[x] = (r8 << 16) | (g8 << 8) | (b8);
+		}
+
+		ra += rd;
+		ga += gd;
+		ba += bd;
+
+	}
+}
+
+void draweffy(SDL_Surface* image)
+{
+	float aaika = SDL_GetTicks();
+	SDL_FillRect(image, NULL, 0x404040);
+	{
+		float xx = sin(aaika*0.00192f);
+		float yy = cos(aaika*0.00553f);
+		xx += sin(aaika*0.00435f);
+		yy += sin(aaika*0.00336f);
+		xx += cos(aaika*0.00153f);
+		yy += cos(aaika*0.00151f);
+
+		xx *= screensiz_w/6;
+		yy *= screensiz_h/6;
+
+		SDL_Rect rect;
+
+		rect.x = screensiz_w/2 - 20 + xx/2;
+		rect.y = screensiz_h/2 - 20 + yy/2;
+		rect.w = 40;
+		rect.h = 40;
+		SDL_FillRect(image, &rect, 0x202020);
+
+		rect.x = screensiz_w/2 - 18 + xx;
+		rect.y = screensiz_h/2 - 18 + yy;
+		rect.w = 36;
+		rect.h = 36;
+		SDL_FillRect(image, &rect, 0x0);
+
+		rect.x = screensiz_w/2 - 16 + xx;
+		rect.y = screensiz_h/2 - 16 + yy;
+		rect.w = 32;
+		rect.h = 32;
+		//SDL_FillRect(image, &rect, rand());
+		gradientrect(image, &rect);
+	}
+}
 
 int main(int argc, char *argv[])
 {
+	// open HLSUDP context
     ctx = hlsudp_open();
     printf("ctx:  %p\n", ctx);
 
 
+    // sdl init crap
 	SDL_Init(SDL_INIT_VIDEO);
 
 	SDL_Window *window = SDL_CreateWindow("base", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screensiz_w*scale, screensiz_h*scale, 0);
@@ -67,13 +149,14 @@ int main(int argc, char *argv[])
 	SDL_Surface *screen = SDL_GetWindowSurface(window);
 
 	SDL_Surface *image = SDL_CreateRGBSurface(0, screensiz_w, screensiz_h, 32, 0,0,0,0);
-	uint16_t HDRimage[screensiz_w*screensiz_h*3];
+
 
 	int lasttime = 0;
 	int frames = 0;
 
 	while (1)
 	{
+		// read events etc
 		SDL_Event event;
 		SDL_PollEvent(&event);
 		if (event.type == SDL_QUIT) {
@@ -86,14 +169,18 @@ int main(int argc, char *argv[])
 
 		const Uint8* 	keystates = SDL_GetKeyboardState(NULL);
 
+		// wait atleast 16msec between frames ~= 60fps
+		#define FRAMETIME 16
 		static int alasttime = 0;
-		while((SDL_GetTicks() - alasttime) < 16)
+		while((SDL_GetTicks() - alasttime) < FRAMETIME)
 			continue;
 		alasttime = SDL_GetTicks();
+
 
 		int aika = SDL_GetTicks();
 		frames++;
 
+		// print fps meter every second
 		if ((aika-lasttime) > 1000.f)
 		{
 			float fps = (float)frames / ((float)(aika-lasttime) / 1000.f);
@@ -102,64 +189,14 @@ int main(int argc, char *argv[])
 			printf("fps: %.2f\n", fps);
 		}
 
+		// draw effy
+		draweffy(image);
 
-		static int aaika = 0;
-
-		if (!keystates[SDL_SCANCODE_SPACE])
-			aaika = aika;
-
-
-
-#if 0
-		//#pragma omp parallel for schedule(dynamic, 8)
-		for (int y = 0; y < screensiz_h; y++)
-		{
-			uint32_t *pixels = (uint32_t *)(image->pixels + y * image->pitch);
-
-			for (int x = 0; x < screensiz_w; x++)
-			{
-				pixels[x] = (x*((y+aaika/8)%90));
-			}
-
-		}
-#endif
-		SDL_FillRect(image, NULL, 0x404040);
-		{
-			float xx = sin(aaika*0.00192f);
-			float yy = cos(aaika*0.00553f);
-			xx += sin(aaika*0.00435f);
-			yy += sin(aaika*0.00336f);
-			xx += cos(aaika*0.00153f);
-			yy += cos(aaika*0.00151f);
-
-			xx *= screensiz_w/6;
-			yy *= screensiz_h/6;
-
-			SDL_Rect rect;
-
-			rect.x = screensiz_w/2 - 20 + xx/2;
-			rect.y = screensiz_h/2 - 20 + yy/2;
-			rect.w = 40;
-			rect.h = 40;
-			SDL_FillRect(image, &rect, 0x202020);
-
-			rect.x = screensiz_w/2 - 18 + xx;
-			rect.y = screensiz_h/2 - 18 + yy;
-			rect.w = 36;
-			rect.h = 36;
-			SDL_FillRect(image, &rect, 0x0);
-
-			rect.x = screensiz_w/2 - 16 + xx;
-			rect.y = screensiz_h/2 - 16 + yy;
-			rect.w = 32;
-			rect.h = 32;
-			SDL_FillRect(image, &rect, rand());
-		}
-
+		// convert image to HDR and send to panels
 		sendframe(image);
 
+		// blit to screen for preview
 		SDL_BlitScaled(image, NULL, screen, NULL);
-
 		SDL_UpdateWindowSurface(window);
 
 	}
