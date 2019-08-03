26e5446382d80fd22ad30b0171cbf871f7adaff7
Eero af Heurlin <eero.afheurlin@iki.fi>
2019-08-03 23:16:15 +0300

    Add flat 1% grey background

--- python/30x30_squares.py
+++ python/30x30_squares.py
@@ -12,10 +12,12 @@ COLOR_MAX = 2**16 - 1
 RED = np.array((COLOR_MAX, 0, 0), dtype=np.uint16)
 GREEN = np.array((0, COLOR_MAX, 0), dtype=np.uint16)
 BLUE = np.array((0, 0, COLOR_MAX), dtype=np.uint16)
+GREY = np.array((COLOR_MAX / 100, COLOR_MAX / 100, COLOR_MAX / 100), dtype=np.uint16)
 
 a = hlslib.PanelArray('panelconfig.txt')
 # remember numpy and y,x addressing
 
+a.pixeldata[:, :] = GREY
 a.pixeldata[-30:, -30:] = BLUE
 a.pixeldata[-60:-30, -60:-30] = GREEN
 a.pixeldata[-90:-60, -90:-60] = RED
